2dfff62625069f75af5527cd26f2e9e412c3e25a
guido2288 <guido2288@gmail.com>
2023-11-04 17:59:03 -0300

    Auth Functionality - Appwrite

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -10,6 +10,7 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { SignupValidation } from "@/lib/validation"
 import { Loader } from "lucide-react"
+import { createUserAccount } from "@/lib/appwrite/api"
 
  
 
@@ -31,7 +32,9 @@ const SignupForm = () => {
    
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
-    //const newUser = await createUserAccount(values);
+    const newUser = await createUserAccount(values);
+
+    console.log(newUser)
   }
   
 
