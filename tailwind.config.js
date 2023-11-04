81ddb217480bd1e6479604a7aad2fa6dec0b9001
guido2288 <guido2288@gmail.com>
2023-11-04 15:26:19 -0300

    Auth Pages addeded

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 
-
-export default {
+module.exports = {
   darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -64,4 +64,3 @@ export default {
   },
   plugins: [require('tailwindcss-animate')],
 };
-
